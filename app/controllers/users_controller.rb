9bdbb37497a43092d3f1925546561d978ec3160d
Pete Eichman <peichman14@gmail.com>
2016-10-20 10:53:12 -0400

    Gave user's the option to edit or delete their own account. Restricted non-admins from deleting other user's accounts. Added a sign in with Facebook link. Fixed user route issue.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def new
+    @user = User.new
+  end
+
   def index
+    @users = User.all
   end
 
   def show
@@ -18,7 +23,37 @@ class UsersController < ApplicationController
     redirect_to user_path(user)
   end
 
-  def delete
+  def destroy
+    @user = User.find(params[:id])
+    if authorized_party
+      if @user.valid?
+        user_valid_destroy
+      else
+        flash[:errors] = "User Account was not deleted."
+        render user_path(@user)
+      end
+    else
+      render(file: File.join(Rails.root, 'public/403.html'), status: 403, layout: false)
+    end
   end
 
+  private
+
+  def authorized_party
+    current_user.try(:admin?) || current_user == @user
+  end
+
+  def admin_redirect
+    if current_user.admin
+      redirect_to users_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def user_valid_destroy
+    @user.destroy
+    flash[:success] = "User Account successfully closed"
+    admin_redirect
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "static_pages#index"
   devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions', omniauth_callbacks: 'callbacks' }
   resources :users
+
+
 end
