377cb4898a0cd788a7cb9ed87f576c3488e8ee9f
Pete Eichman <peichman14@gmail.com>
2016-10-27 17:53:43 -0400

    Fixed problems database and models for Twilio. Twilio now works for my phone number, but no t for unverified numbers. Need to purchase a Twilio number for it to work with other numbers.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(first_name: "Tester", last_name: "St.Testler", email: "testing1@gmail.com", password: "testing", admin: true)
+User.create(first_name: "Tester", last_name: "St.Testler", email: "testing1@gmail.com", password: "testing", phone_number: "508-333-3523" admin: true)
 
 User.create(first_name: "Test-Girl", last_name: "St.Testler", email: "testing2@gmail.com", password: "testing")
 
