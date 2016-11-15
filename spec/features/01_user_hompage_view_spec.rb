56e358348309e22208b9d881b233fb04789b78f3
Pete Eichman <peichman14@gmail.com>
2016-11-15 18:11:00 -0500

    Updated test for homepage. Added test for deleting parking passes from profile page.

--- spec/features/01_user_hompage_view_spec.rb
+++ spec/features/01_user_hompage_view_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
-feature "user visits homepage" do
-  context "and as a user" do
-    scenario "I see a page" do
+feature "User visits the home page" do
+  context "As an unauthenticated user" do
+    scenario "I can view the home page" do
 
       visit '/'
 
-      expect(page).to have_content("Welcome to ParkMe!")
+
+      expect(page).to have_content "Welcome to ParkMe!"
+      expect(page).to have_content "Please Sign In Below:"
+      expect(page).to have_field "user[email]"
+      expect(page).to have_field "user[password]"
+      expect(page).to have_button "Sign in"
+      expect(page).to have_link "Forgot your password?"
+      expect(page).to have_content "OR"
+      expect(page).to have_link "Sign up"
+      expect(page).to have_link "Sign in with Facebook"
     end
   end
 end
