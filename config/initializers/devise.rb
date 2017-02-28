c725ebe0c7f20086e318b15322fbc466f61749f0
Pete Eichman <peichman14@gmail.com>
2017-02-28 18:42:19 -0500

    precompiling assets for heroic

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,6 +11,7 @@ Devise.setup do |config|
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
   config.reset_password_within = 6.hours
   config.sign_out_via = :delete
+  config.secret_key = '0aa923e865db5b95edb34f82bbcd5b93ec28a017e7316ec3c2370bc7a5e02702a74fe806684e7208e0a87f075e04bbb84b619a0dbaaef54493b6dc5921ed8075'
 
   config.omniauth :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET'], scope: 'email'
 end
